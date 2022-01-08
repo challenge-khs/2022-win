a9751f49b26dd045ba5cc66e28a155e70f85d1dd
challenge-khs <61410023+challenge-khs@users.noreply.github.com>
2022-01-08 23:02:35 +0900

    Create w1_4.java

---
+++ Java_exercise/w1_4.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class w1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, m, k, temp = 0;
+		int arr[][] = new int[5][5];
+		int p[] = new int[4];
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 4; i++)
+		{
+			p[i] = 0;
+		}
+		
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				arr[i][j] = sc.nextInt();
+				//temp[i][j] = arr[i][j];
+			}
+		}
+		k = sc.nextInt();
+		for (int z = 0; z < k; z++)
+		{
+			n = sc.nextInt();
+			m = sc.nextInt();
+
+			n = n - 1;
+			m = m - 6;
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					if (i == m)
+					{
+						temp = arr[i][j];
+						arr[i][j] = arr[j][n];
+						arr[j][n] = temp;
+					}
+				}
+			}
+		}
+
+		/*for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				cout << arr[i][j] << " ";
+			}
+			cout << "\n";
+		}*/
+
+		temp = 0;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (i < 2 && j < 2)
+					p[0] += arr[i][j];
+				else if (i < 2 && j > 2)
+					p[1] += arr[i][j];
+				else if (i > 2 && j < 2)
+					p[2] += arr[i][j];
+				else if (i > 2 && j > 2)
+					p[3] += arr[i][j];
+				else
+					temp += arr[i][j];
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < 4; i++)
+		{
+			p[i] += (temp / 4);
+			System.out.print(p[i]+" ");
+		}
+		System.out.println();
+	}
+
+}
