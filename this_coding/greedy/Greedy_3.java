2d340d4a4365c9af320a758fc28714529bbfa704
challenge-khs <61410023+challenge-khs@users.noreply.github.com>
2022-01-20 23:08:07 +0900

    Create Greedy_3.java

---
+++ this_coding/greedy/Greedy_3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Greedy_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, k, count = 0;
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		k = sc.nextInt();
+		
+		while(true) {
+			if (n == 1) 
+				break;
+			if(n % k == 0) 
+				n /= k;
+			else 
+				n--;
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
