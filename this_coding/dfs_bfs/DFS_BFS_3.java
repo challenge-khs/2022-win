83effcb12e5d1beba18d54614e9e39e7999756d1
challenge-khs <61410023+challenge-khs@users.noreply.github.com>
2022-01-24 23:14:55 +0900

    Create DFS_BFS_3.java

---
+++ this_coding/dfs_bfs/DFS_BFS_3.java
@@ -0,0 +1,12 @@
+public class DFS_BFS_3 {
+
+	public static void recursiveFunc() {
+		System.out.println("재귀 함수 호출");
+		recursiveFunc();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		recursiveFunc();
+	}
+
+}
