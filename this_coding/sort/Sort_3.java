565e078e0f1ca3b3b2b81d553b23e6f28683bbee
challenge-khs <61410023+challenge-khs@users.noreply.github.com>
2022-01-27 03:17:00 +0900

    Create Sort_3.java

---
+++ this_coding/sort/Sort_3.java
@@ -0,0 +1,24 @@
+//insertion sort
+public class Sort_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {7,5,9,0,3,1,6,2,4,8};
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = i; j > 0; j--) {
+				if(arr[j] < arr[j-1]) {
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}
+				else
+					break;
+			}
+		}
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
