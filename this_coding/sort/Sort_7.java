740e5f047fd9f621446200ac652f65a76d36872d
challenge-khs <61410023+challenge-khs@users.noreply.github.com>
2022-01-28 17:28:28 +0900

    Create Sort_7.java

---
+++ this_coding/sort/Sort_7.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Sort_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {7,5,9,0,3,1,6,2,4,8};
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+}
