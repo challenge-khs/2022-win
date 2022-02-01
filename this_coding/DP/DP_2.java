c299ce2c790857fcbcbd3a6e2f2fc2af5373b69b
challenge-khs <61410023+challenge-khs@users.noreply.github.com>
2022-02-01 22:52:48 +0900

    Create DP_2.java

---
+++ this_coding/DP/DP_2.java
@@ -0,0 +1,19 @@
+public class DP_2 {
+
+	public static long[] d = new long[100];
+	
+	public static long fibo(int x) {
+		if(x == 1 || x == 2)
+			return 1;
+		if (d[x] != 0) 
+			return d[x];
+		d[x] = fibo(x-1) + fibo(x-2);
+		return d[x];
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(fibo(50));
+	}
+
+}
